dd85fcc4fa40059332b77b7f124385b5c7c8adda
Kawsar Ahmed <kaws.med@gmail.com>
2023-09-05 15:44:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.querySelector('#addTask').addEventListener("click", function(){
+    var itemValue = document.querySelector("#new-task").value.trim();
+    var listItem = document.createElement("li");
+    listItem.className = "item";
+    listItem.innerText = itemValue;
+
+    var items = document.querySelector("#items");
+    items.append(listItem);
+});
+
+
+// document.querySelector('.delete').addEventListener("click", function(evt){
+//     evt.target.closest('li').remove();
+// });
+
+document.querySelector('.complete-list ul').addEventListener("click", function(evt){
+    evt.target.closest('li').remove();
+});
+
+document.getElementById('items').addEventListener('click', function (event) {
+    if (event.target.classList.contains('item')){
+        var text = event.target.textContent;
+
+        var listItem = document.createElement("li");
+        listItem.className = "item";
+        listItem.innerText = text;
+
+        var btn = document.createElement("button");
+        btn.className = "delete";
+        btn.innerText = "Delete";
+        listItem.append(btn);
+
+        var list = document.querySelector(".complete-list ul");
+        list.append(listItem);
+        
+        var element = event.target.closest('li');
+        element.remove();
+    }
+  });
